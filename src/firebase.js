0a0044a4e16b172a594ec00ca7189f2ca7310257
krishnagottipalli <saikrishna.gdsv@gmail.com>
2020-10-09 21:26:56 +0530

    amazon clone header finished

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC8_GZja-yVYcWTHKIaAYk6SMRhBgpAgc0",
+    authDomain: "clone-8e701.firebaseapp.com",
+    databaseURL: "https://clone-8e701.firebaseio.com",
+    projectId: "clone-8e701",
+    storageBucket: "clone-8e701.appspot.com",
+    messagingSenderId: "240787507608",
+    appId: "1:240787507608:web:88967f798757c6c839c3ed",
+    measurementId: "G-G48XMYT3C2"
+  };
